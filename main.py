a6618ecf8f2d476c05f6c7de5d679a7c5145f249
pepab0t <cernikpepa@seznam.cz>
2023-06-11 10:13:31 +0200

    new register and login

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from application import create_app, DB_NAME
 
 def main():
     socketio, app = create_app(f"sqlite:///{DB_NAME}")
-    socketio.run(app, host="0.0.0.0", port=5000, debug=False)
+    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
 
 
 if __name__ == "__main__":
